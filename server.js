ecf68dbf2b9cbfca454ce38ff1d06af41dfdd849
viettc <viettc@vng.com.vn>
2019-02-14 16:25:55 +0700

    fix bug

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
-const PORT = 3484;									
+const PORT = 3484									
  
 var http = require('http') 							
 var socketio = require('socket.io')				
-var ip = require('ip');
+var ip = require('ip')
 var app = http.createServer(function(req, res) {
     res.write("hello")
     res.end()
-});					
-var io = socketio(app);
-app.listen(process.env.PORT || PORT);										
+})					
+var io = socketio(app)
+app.listen(process.env.PORT || PORT)									
 console.log("Server running...")
  
 io.on('connection', function(socket) {	
-    console.log("Connected"); 
+    console.log("Connected")
 	
     socket.emit('welcome', {
         message: 'Connected !!!!'
-    });
+    })
 	
     socket.on('connection', function(message) {
-        console.log(message);
-    });
-});
+        console.log(message)
+    })
+
+    socket.on('move', function(message) {
+        console.log(message)
+    })
+})
