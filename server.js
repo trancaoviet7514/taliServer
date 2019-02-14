8c562de9bfb39917dc5e8ec2214fd66eff6440e0
viettc <viettc@vng.com.vn>
2019-02-14 15:18:56 +0700

    fix port

--- server.js
+++ server.js
@@ -8,24 +8,6 @@ var io = socketio(app);
 app.listen(process.env.PORT || PORT);										
 console.log("Server running...")
  
-function ParseJson(jsondata) {
-    try {
-        return JSON.parse(jsondata);
-    } catch (error) {
-        return null;
-    }
-}
- 
-function sendTime() {
-	
-	var json = {
-        ESP8266: 12,									
-		soPi: 3.14,										
-		time: new Date()							
-    }
-    io.sockets.emit('atime', json);
-}
- 
 io.on('connection', function(socket) {	
     console.log("Connected"); 
 	
@@ -35,15 +17,5 @@ io.on('connection', function(socket) {
 	
     socket.on('connection', function(message) {
         console.log(message);
-    });
-	
-    socket.on('atime', function(data) {
-        sendTime();
-        console.log(data);
-    });
-	
-	socket.on('arduino', function (data) {
-	  io.sockets.emit('arduino', { message: 'R0' });
-      console.log(data);
     });
 });
