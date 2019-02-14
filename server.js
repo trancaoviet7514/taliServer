853234c7dd25c1147fdb211fd7b45f7740c03b70
viettc <viettc@vng.com.vn>
2019-02-14 15:42:42 +0700

    fix response bug

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ var socketio = require('socket.io')
 var ip = require('ip');
 var app = http.createServer();					
 var io = socketio(app);								
-app.listen(process.env.PORT || PORT);										
+app.listen(process.env.PORT || PORT, function (req, res) {
+    res.end();
+});										
 console.log("Server running...")
  
 io.on('connection', function(socket) {	
